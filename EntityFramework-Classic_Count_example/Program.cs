417f8f4839be35719ad89917afc9c7bb35c712cc
Mat3o <matlagit@outlook.com>
2020-09-15 18:06:49 +0200

    bug case with new[]

--- EntityFramework-Classic_Count_example/Program.cs
+++ EntityFramework-Classic_Count_example/Program.cs
@@ -1,6 +1,5 @@
 ﻿using EntityFramework_Classic_Count_example.DALs;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace EntityFramework_Classic_Count_example
@@ -14,7 +13,7 @@ namespace EntityFramework_Classic_Count_example
             var workingPureEFCore = service.GetAllElements()
                 .Where(p => (p.Name == "test"))
                 .Where(p => p.Active)
-                .Where(p => new List<int>() { 1, 2, 3, 4 }.Contains(p.Id))
+                .Where(p => new []{ 1, 2, 3, 4 }.Contains(p.Id))
                 .GroupBy(p => new { p.Id, p.Name })
                 .Select(p => new { p.Key.Id, p.Key.Name, Counts = p.Count() })
                 .ToList();
@@ -22,7 +21,7 @@ namespace EntityFramework_Classic_Count_example
             var notWorkingZEFClassic = service.GetAllElements().Execute<object>(
                 "Where(p => (p.Name== \"test\"))" +
                 ".Where(p => p.Active)" +
-                ".Where(p => new List<int>() { 1, 2, 3, 4 }.Contains(p.Id))" +
+                ".Where(p => new []{ 1, 2, 3, 4 }.Contains(p.Id))" +
                 ".GroupBy(p => new { p.Id, p.Name})" +
                 ".Select(p => new { p.Key.Id, p.Key.Name, Counts =  p.Count() })" +
                 ".ToList()");
